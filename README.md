48cf51b47b64ac66c863b8bc6048f402836f57a8
Hemchand-King <70268346+Hemchand-King@users.noreply.github.com>
2020-10-24 08:33:30 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Rename-V2.4
+## TGX File Renamer Bot
+---
+
+**My Feature**:
+
+👉 Rename any Telegram Files with Custom Thumbnail Support
+
+### Installation
+
+#### The Easy Way
+
+### You can also tap the Deploy To Heroku button below to deploy straight to Heroku!
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://www.heroku.com/deploy?template=https://github.com/Chris-Carlo/TGXRenamerBot)
+
+#### The Hard Way
+
+```sh
+virtualenv -p python3 VENV
+. ./VENV/bin/activate
+pip install -r requirements.txt
+cp sample_config.py config.py
+--- EDIT config.py values appropriately ---
+python bot.py
+```
+
+#### LICENSE
+- GPLv3
+
---
+++ translation.py
@@ -0,0 +1,107 @@
+class Translation(object):
+    START_TEXT = """Hai,
+
+<code>I'm a Simple File Renamer bot and can upload as File or Video With Permanent Thumbnail support!💯.</code> \n\n<b>Bot Created By:</b> <a href='https://t.me/Chris_Carlo'>Chris Carlo 👨‍🎤</a>
+
+Press help button which is available at the bottom to know how to use me."""
+
+    START_TEXT_1 = """Hai,
+
+<code> I'm a Simple File Renamer bot and can upload as File or Video With Permanent Thumbnail support!💯.</code> \n\n<b>Bot Created By:</b> <a href='https://t.me/Darklester'>Chris_Carlo'>Chris Carlo 👨‍🎤</a>
+
+Press help button which is available at the bottom to know how to use me."""
+
+    RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
+    ABS_TEXT = " Please don't be selfish."
+    UPGRADE_TEXT = "There is no upgrade plan till now it will be added in future"
+    DOWNLOAD_START_VIDEO = "<tt>Downloading to my server....⬇️</tt>"
+    DOWNLOAD_START = "<tt>Downloading to my server....⬇️</tt>"
+    UPLOAD_START_VIDEO = "<tt>Uploading as video...⬆️</tt>"
+    UPLOAD_START = "<tt>Uploading as File...⬆️</tt>"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations.I can't do anything for that 😢."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using [TECHNOGRAM X](https://t.me/TGXTECH)'s bot.**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
+    NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
+    SAVED_CUSTOM_THUMB_NAIL = "Custom File thumbnail SAVED ✅️. This image will be used in the File."
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
+    SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
+    CUSTOM_CAPTION_UL_FILE = "<b>{}</b> \n\n@TGXTECH"
+    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    HELP_USER = """<b>Hey! Here you can know how to use me</b>, 
+    
+<tt>You Need help ?</tt>
+    
+1.◽<b>Send me any Telegram File or Video.</b>
+
+2.◽<b>Reply to that message with</b> <code>/rename New file name.extnsion</code> (custom thumbnail support) (it will UPLOAD AS FILE)
+
+3.◽<b>Reply to that message with</b> <code>/rename_video New file name.extensiom</code> (Custom Thumbnail support) (it will UPLOAD AS STREAMBLE VIDEO)
+
+4.◽<b>Send A Photo to make it as permanent Thumbnail</b>   
+ 
+<b>❤️ Made With Love:======= </b> @Chris_Carlo & @Darklester.
+
+<b>Thanks to My Boss :</b><a href="https://t.me/Darklester">Owner 👨‍🏫</a> and <a href='https://t.me/Chris_Carlo'>Chris Carlo 👨‍🎤</a>
+
+
+🗣𝐃𝐢𝐬𝐜𝐮𝐬𝐬 𝐆𝐫𝐨𝐮𝐩: @TGXTECHGroup
+📢𝐔𝐩𝐝𝐚𝐭𝐞 𝐂𝐡𝐚𝐧𝐧𝐞𝐥: @TGXTECH"""
+
+  
+    REPLY_TO_DOC_FOR_RENAME_FILE = "🤦‍♂️ Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.\n\n(For uploading as file).\n\nSee /help for more information. "
+    REPLY_TO_DOC_FOR_RENAME_VIDEO = "🤦‍♂️ Reply to a Telegram media to `/rename_video New Name.extension` with custom thumbnail support.\n\n(For uploading as video).\n\nSee /help for more information."
+    ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
+    FREE_USER_LIMIT_Q_SZE = """Cannot Process.
+Free users only 1 request per 30 minutes.
+/upgrade or Try 1800 seconds later."""
+    IFLONG_FILE_NAME = """File Name limit allowed by Telegram is {alimit} characters.
+The given file name has {num} characters.
+
+<b>Short your file name please.</b>
+©️ <code>@TGXTECH</code>
+Please short your file name and try again!"""
+    ABOUT = """Hi {},
+
+🤖 My Name: [TGX Renamer Bot](t.me/RenamerTGXBOT)
+    
+📝 Language: [Python 3](https://www.python.org/)
+
+📫 Framework: [Pyrogram](https://github.com/pyrogram/pyrogram)
+
+👨‍💻 Developer: [Chris Carlo](https://t.me/Chris_Carlo)
+
+📢 Channel: [TECHNOGRAM X](https://t.me/TGXTECH)
+
+👥 Group: [TGX SUPPORT GROUP](https://t.me/TGXTECHGroup)
+
+⚖️ Credit: `Everyone in this journey`
+
+🌎 Server: [HEROKU](https://heroku.com)
+
+
+🛡️ Powered By: [DARKLESTER](https://t.me/Darklester)"""
+
+
+
+    ABOUT_1 = """**MY DETAILS :**
+🤖 My Name: [TGX Renamer Bot](t.me/RenamerTGXBOT)
+    
+📝 Language: [Python 3](https://www.python.org/)
+
+📫 Framework: [Pyrogram](https://github.com/pyrogram/pyrogram)
+
+👨‍💻 Developer: [Chris Carlo](https://t.me/Chris_Carlo)
+
+📢 Channel: [TECHNOGRAM X](https://t.me/TGXTECH)
+
+👥 Group: [TGX SUPPORT GROUP](https://t.me/TGXTECHGroup)
+
+⚖️ Credit: `Everyone in this journey`
+
+🌎 Server: [HEROKU](https://heroku.com)
+
+
+🛡️ Powered By: [DARKLESTER](https://t.me/Darklester)"""
